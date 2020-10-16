2fff824db9cc1b36928f10f569ee379ff374cf22
yuyaamano23 <nba.chako.nba@icloud.com>
2020-10-16 22:40:39 +0900

    記事詳細のapi受け取り

--- src/resources/js/components/CompanyCardList.js
+++ src/resources/js/components/CompanyCardList.js
@@ -7,66 +7,19 @@ class CompanyCardList extends Component {
         super(props);
         this.state = {
             articles: [],
-
-            // 仮データ
-            // reviewData: [
-                // {
-                    // id: "1",
-                    // company: "Hamee",
-                    // term: "1週間",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風はああああああああああああああaaaaaaaaaaaaa"
-                // },
-                // {
-                    // id: "2",
-                    // company: "Yahoo!",
-                    // term: "3週間",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風はbbbbbbbbbbbbbbbbbbb"
-                // },
-                // {
-                    // id: "3",
-                    // company: "Google",
-                    // term: "1ヶ月",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風は..."
-                // },
-                // {
-                    // id: "4",
-                    // company: "Hamee",
-                    // term: "1週間",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風は..."
-                // },
-                // {
-                    // id: "5",
-                    // company: "Yahoo!",
-                    // term: "3週間",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風は..."
-                // },
-                // {
-                    // id: "6",
-                    // company: "Google",
-                    // term: "1ヶ月",
-                    // task:
-                        // "オフラインで自社で開発しているネクストエンジンの開発に携わりました。社風は..."
-                // }
-            // ]
         };
     }
 
     componentDidMount() {
     //   render直後に行いたい処理を書くところ
-      const url = "http://localhost/api/articles";
-      axios.get(url).then((res) => {
-          console.log(res.data.articles);
-          this.setState({
-              articles: res.data.articles
-          });
-      },).catch(() => {
-          console.log('通信に失敗しました。')
-      });
+        const url = "http://localhost/api/articles";
+        axios.get(url).then((res) => {
+        this.setState({
+            articles: res.data.articles
+        });
+        },).catch(() => {
+            console.log('通信に失敗しました。')
+        });
     }
 
     render() {
