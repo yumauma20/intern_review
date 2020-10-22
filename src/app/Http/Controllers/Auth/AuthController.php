fb0dd88adfd7eec15768cb27a07f6bb67d204d17
yumauma20 <71yuma10@gmail.com>
2020-10-22 18:06:28 +0900

    JWT認証機能の変更
    公式に則ってやりやした。
    auth()をauth('api')に

--- src/app/Http/Controllers/Auth/AuthController.php
+++ src/app/Http/Controllers/Auth/AuthController.php
@@ -2,41 +2,82 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Auth\AuthManager;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-  use AuthenticatesUsers;
-
-  protected $redirectTo = '/home';
-  private $authManager;
-
-  public function __construct(AuthManager $authManager)
-  {
-    $this->authManager = $authManager;
-    $this->middleware('guest')->except('logout');
-  }
-
-  public function login(Request $request): JsonResponse
-  {
-    $guard = $this->authManager->guard('api');
-    $token = $guard->attempt([
-      'email' =>  $request->get('email'),
-      'password'  =>  $request->get('password'),
-    ]);
-    if (!$token) {
-      return new JsonResponse(__('auth.failed'));
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
     }
-    return new JsonResponse($token);
-  }
 
-  public function logout()
-  {
-    auth()->logout();
-    return response()->json(['message' => 'Successfully logged out']);
-  }
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth('api')->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth('api')->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth('api')->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60
+        ]);
+    }
 }
