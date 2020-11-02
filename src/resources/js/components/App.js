e597b7952f3d024507f15c45e8b8195cdff46419
yuyaamano23 <nba.chako.nba@icloud.com>
2020-11-02 22:04:59 +0900

    urlか#マークを滅ぼす

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import PostReviewPage from "../pages/PostReviewPage";
 import SignUpPage from "../pages/SignUpPage";
@@ -14,11 +14,14 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <Route exact path="/" component={MainPage} />
-                <Route path="/SignUp" component={SignUpPage} />
-                <Route path="/Login" component={LoginPage} />
-                <Route path="/Post" component={PostReviewPage} />
-                <Route path="/Detail/:id" component={DetailPage} />
+                <Switch>
+                    <Route exact path="/" component={MainPage} />
+                    <Route path="/SignUp" component={SignUpPage} />
+                    <Route path="/Login" component={LoginPage} />
+                    <Route path="/Post" component={PostReviewPage} />
+                    {/* <Route path="/Mypage" component={MyPage} /> */}
+                    <Route path="/Detail/:id" component={DetailPage} />
+                </Switch>
             </Router>
         );
     }
