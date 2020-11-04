c6d4773a64232d6bf02773fe78b9e5f9abfb9478
yumauma20 <71yuma10@gmail.com>
2020-11-05 00:53:02 +0900

    ログアウト機能実装 Redux ver

--- src/resources/js/components/LogoutForm.js
+++ src/resources/js/components/LogoutForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutAction } from "../logoutAction";
 
 class LogoutForm extends Component {
     render() {
@@ -8,15 +10,24 @@ class LogoutForm extends Component {
             .post(url)
             .then(res => {
                 axios.defaults.headers.common["Authorization"] = "";
-                console.log(axios.defaults.headers.common["Authorization"]);
                 console.log("ログアウト成功");
+                this.props.logoutAction();
             })
             .catch(res => {
-                console.log(axios.defaults.headers);
                 console.log("ログアウト失敗");
             });
         return <Redirect to="/" />;
     }
 }
 
-export default LogoutForm;
+const mapStateToProps = state => ({
+    LoggedIn: state.LoggedIn
+});
+
+const mapDispatchToProps = dispatch => ({
+    logoutAction: () => dispatch(logoutAction())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogoutForm);
+
+// export default LogoutForm;
