a547c7dc6789f995305351c30b2884935777d88c
yumauma20 <71yuma10@gmail.com>
2020-11-05 21:28:10 +0900

    policyのリファクタリング

--- src/app/Policies/ArticlePolicy.php
+++ src/app/Policies/ArticlePolicy.php
@@ -11,40 +11,6 @@ class ArticlePolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any articles.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the article.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Article  $article
-     * @return mixed
-     */
-    public function view(User $user, Article $article)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create articles.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the article.
      *
      * @param  \App\User  $user
@@ -66,29 +32,5 @@ class ArticlePolicy
     public function delete(User $user, Article $article)
     {
         return $user->id === $article->user_id;
-    }
-
-    /**
-     * Determine whether the user can restore the article.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Article  $article
-     * @return mixed
-     */
-    public function restore(User $user, Article $article)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the article.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Article  $article
-     * @return mixed
-     */
-    public function forceDelete(User $user, Article $article)
-    {
-        //
     }
 }
