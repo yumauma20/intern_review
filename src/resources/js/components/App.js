bfef49a75586374081182b8cb577017a7d6fd564
yumauma20 <71yuma10@gmail.com>
2020-11-10 02:31:14 +0900

    BrowserRooterに修正

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { HashRouter as Router, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import PostReviewPage from "../pages/PostReviewPage";
 import SignUpPage from "../pages/SignUpPage";
@@ -17,18 +17,18 @@ import store, { persistor } from "../store";
 class App extends Component {
     render() {
         return (
-            <Router>
-                {/* <Switch> */}
-                <Route exact path="/" component={MainPage} />
-                <Route path="/SignUp" component={SignUpPage} />
-                <Route path="/Login" component={LoginPage} />
-                <Route path="/Logout" component={LogoutPage} />
-                <Route path="/Post" component={PostReviewPage} />
-                <Route path="/MyPage" component={MyPage} />
-                <Route path="/Detail/:id" component={DetailPage} />
-                <Route path="/MyDetail/:id" component={MyDetailPage} />
-                {/* </Switch> */}
-            </Router>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={MainPage} />
+                    <Route path="/SignUp" component={SignUpPage} />
+                    <Route path="/Login" component={LoginPage} />
+                    <Route path="/Logout" component={LogoutPage} />
+                    <Route path="/Post" component={PostReviewPage} />
+                    <Route path="/MyPage" component={MyPage} />
+                    <Route path="/Detail/:id" component={DetailPage} />
+                    <Route path="/MyDetail/:id" component={MyDetailPage} />
+                </Switch>
+            </BrowserRouter>
         );
     }
 }
