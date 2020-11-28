0795e5c76cfee31893e5f534b2759e09e8281937
yumauma20 <71yuma10@gmail.com>
2020-11-28 17:03:09 +0900

    hashrooter削除

--- src/resources/js/components/App.js
+++ src/resources/js/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import { HashRouter as Router, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import PostReviewPage from "../pages/PostReviewPage";
 import SignUpPage from "../pages/SignUpPage";
